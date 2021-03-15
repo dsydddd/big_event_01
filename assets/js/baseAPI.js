8343bca101422c2aa7fb2b523ce0275a45a7cf30
dingshangyu <1352837412@qq.com>
2021-03-15 10:09:19 +0800

    主页功能完成

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -7,4 +7,21 @@ $.ajaxPrefilter(function (options) {
     // 手动为 url 添加路径前缀
      console.log('http://api-breakingnews-web.itheima.net' + options.url)
     options.url = baseURL + options.url
+    // 如果带my的url 提出
+    if (options.url.indexOf("/my/") !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+    options.complete = function (res) {
+        console.log(res.responseJSON);
+        var obj = res.responseJSON;
+        if (obj.status == 1 && obj.message === "身份认证失败!") {
+            localStorage.removeItem("token");
+            location.href ="/login.html"
+        }
+        
+        
+    }
+
 });
